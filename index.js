63f4b4b4d2c7a6191ae8ee093a5ae97ef2f6d8ee
Monish B <31185862+bmonish@users.noreply.github.com>
2024-01-24 18:36:15 +0000

    Linted Code

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/status", (req,res) => {
-  res.json("Server Running")
-})
+app.get('/status', (req, res) => {
+  res.json('Server Running');
+});
 
 // eslint-disable-next-line no-console
 app.listen(3000, () => console.log('Server Started on Port 3000'));
